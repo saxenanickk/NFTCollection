c1d349352032457928ed367409892877c77a169b
saxenanickk <saxenanickk@gmail.com>
2022-10-14 22:55:27 +0530

    Collection screen UI completed

---
+++ src/screens/collection/index.tsx
@@ -0,0 +1,157 @@
+import {View, FlatList, ListRenderItem, ActivityIndicator} from 'react-native';
+import React, {FC, useReducer, useState} from 'react';
+import {hScale, wScale} from '../../utils';
+import NFTCard from '../../components/nftCard';
+import {styles} from './styles';
+import {EmptyView} from '../../components/emptyView';
+import {initialState, reducer} from '../../data/reducer';
+
+const ItemSeparatorComponent: FC = () => <View style={{height: hScale(10)}} />;
+
+const DATA = [
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '1',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/1-the_eth_saint.png',
+    name: 'Rockstars of EPNS V2 #1',
+    description: 'Indian saint medit-ETHing',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '10',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/10-meethai.png',
+    name: 'Rockstars of EPNS V2 #10',
+    description: 'A bowl of sweet gems for web3 enthusiasts',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '100',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/100-push_chatri.png',
+    name: 'Rockstars of EPNS V2 #100',
+    description: 'Saving your collection from bad weather',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '11',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/11-etheyyam.png',
+    name: 'Rockstars of EPNS V2 #11',
+    description:
+      'Theyyam is a popular ritual form of dance worship in Kerala and Karnataka',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '12',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/12-kanha.png',
+    name: 'Rockstars of EPNS V2 #12',
+    description:
+      'Baby avtar of Lord Krishna enjoying his favourite meal (Maakhan)',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '13',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/13-vicious_lady_serpent.png',
+    name: 'Rockstars of EPNS V2 #13',
+    description:
+      'Vicious Lady Serpent  on the lookout for her next 100x shitcoin',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '14',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/14-eth_architect.png',
+    name: 'Rockstars of EPNS V2 #14',
+    description: 'ETH Architect crafting the Ethereum ecosystem',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '15',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/15-rajinikanth.png',
+    name: 'Rockstars of EPNS V2 #15',
+    description: 'One of the most iconic actors from Tamil Cinema',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '16',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/16-Goddess_mahakali.png',
+    name: 'Rockstars of EPNS V2 #16',
+    description: 'Avtar of godess mahakali in bangol’s traditional attire',
+  },
+  {
+    collectionName: 'Rockstars of EPNS V2',
+    collectionAddress: '0xA2b885e7065EA59a3251489715ca80DE5Ff642f8',
+    chain: 'ETH',
+    network: 'mainnet',
+    collectionTokenId: '17',
+    imageUrl:
+      'https://epns.mypinata.cloud/ipfs/QmVr4bSmoic8AH6Tj2Pm5Tak65tqPBg6WifmkAQAbuPZTP/17-dafli_wala.png',
+    name: 'Rockstars of EPNS V2 #17',
+    description: 'Dafli wala playing the beats of Ethereum',
+  },
+];
+
+const Collection = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state.collections);
+  const [loading, setLoading] = useState(true);
+  const renderItem: ListRenderItem<number> | null | undefined = ({item}) => (
+    <NFTCard
+      img={item?.imageUrl}
+      name={item?.name}
+      description={item?.description}
+      address={item?.collectionAddress}
+      tokenId={''}
+    />
+  );
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        contentContainerStyle={{paddingHorizontal: wScale(10)}}
+        ListEmptyComponent={loading ? EmptyView : null}
+        ListFooterComponent={loading ? <ActivityIndicator /> : null}
+        ItemSeparatorComponent={ItemSeparatorComponent}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        data={DATA}
+        renderItem={renderItem}
+      />
+    </View>
+  );
+};
+
+export {Collection};
---
+++ src/screens/collection/styles.ts
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+import {Colors, wScale} from '../../utils';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.WHITE,
+    paddingTop: wScale(10),
+  },
+});
+
+export {styles};
