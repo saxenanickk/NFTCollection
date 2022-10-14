3494d3eb856939a6b645fa3a1540fe7bc2e77ca4
saxenanickk <saxenanickk@gmail.com>
2022-10-14 22:54:48 +0530

    ErrorView component added

---
+++ src/components/errorView/index.tsx
@@ -0,0 +1,16 @@
+import {View} from 'react-native';
+import {hScale, wScale} from '../../utils';
+import {strings} from '../../data/strings/en';
+import {Colors} from '../../utils';
+import {NFTText} from '../nftText';
+import React, {FC} from 'react';
+
+const ErrorView: FC = () => (
+  <View style={{paddingHorizontal: wScale(20)}}>
+    <NFTText color={Colors.ERROR} weight={'bold'} size={hScale(16)}>
+      {strings.error_occured}
+    </NFTText>
+  </View>
+);
+
+export {ErrorView};
